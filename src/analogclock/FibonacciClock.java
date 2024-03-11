83567703cf0b391e8b15c43200d6b6fde5ee44c8
Isaac Santos <isaac.emanuel@live.com>
2024-03-11 00:13:02 -0600

    adjusting minutes arcs

--- src/analogclock/FibonacciClock.java
+++ src/analogclock/FibonacciClock.java
@@ -10,7 +10,7 @@ public class FibonacciClock extends JFrame implements Runnable {
     private Image offScreenImage;
     private Image bgImage, spiralImageRed, spiralImageWhite;
     private Graphics offScreenGraphics;
-    private Font myFont = new Font("Monospaced", Font.BOLD, 20);
+    private Font myFont = new Font("Monospaced", Font.BOLD, 22);
 
     public FibonacciClock() {
         super("Fibonacci Clock");
@@ -77,16 +77,8 @@ public class FibonacciClock extends JFrame implements Runnable {
         if (hour > 12) {
             hour -= 12;
         }
-        // Dot in clock's center
-        int circleRadius = 5;
-        g.setColor(Color.white);
-        g.fillOval(centerX - circleRadius, centerY - circleRadius, circleRadius * 2, circleRadius * 2);
-
-        // Dot inside previous dot
-        int smallCircleRadius = 3;
-        g.setColor(Color.red);
-        g.fillOval(centerX - smallCircleRadius, centerY - smallCircleRadius, smallCircleRadius * 2, smallCircleRadius * 2);
-
+        
+        
         Graphics2D g2d = (Graphics2D) g.create();
         ((Graphics2D) g2d).setStroke(new BasicStroke(2));
 
@@ -115,6 +107,7 @@ public class FibonacciClock extends JFrame implements Runnable {
 
         g.setColor(Color.red);
         g.setFont(myFont);
+        int circleRadius = 5;
         float initialStrokeWidth = 4;
         int radiusFibonacci = Math.min(getWidth(), getHeight()) / 4;
         
@@ -122,32 +115,36 @@ public class FibonacciClock extends JFrame implements Runnable {
         ((Graphics2D) g).setStroke(new BasicStroke(initialStrokeWidth));
         int arcSpacing = 20; // Space between arcs
         g.drawArc(centerX - 150, centerY - 155, radiusFibonacci + arcSpacing * 2, radiusFibonacci + arcSpacing * 2, 30, 50);
-        //g.drawString("10", centerX, centerY - 165);
-        g.drawArc(centerX - 200, centerY - 187 - arcSpacing, radiusFibonacci + 2 * arcSpacing * 3, radiusFibonacci + 2 * arcSpacing * 2, 20, 50);
-        //g.drawString("20", centerX + 50 , centerY - 195);
+        g.drawString("10", centerX + 80, centerY - 70);
+        g.drawArc(centerX - 200, centerY - 187 - arcSpacing, radiusFibonacci + 2 * arcSpacing * 3, radiusFibonacci + 2 * arcSpacing * 2, 25, 50);
+        g.drawString("20", centerX + 110 , centerY - 100);
         g.drawArc(centerX - 420, centerY - 290 - arcSpacing, radiusFibonacci + 3 * arcSpacing * 6, radiusFibonacci + 3 * arcSpacing * 2, 15, 50);
-        //g.drawString("30", centerX + 115, centerY - 185);
+        g.drawString("30", centerX + 135, centerY - 165);
 
         //Bottom lines for fibonacci clock
-        g.drawArc(centerX - 104, centerY - 100, radiusFibonacci + arcSpacing * 2, radiusFibonacci + arcSpacing * 2, 180, 50);
-        g.drawArc(centerX - 135, centerY - 70 - arcSpacing, radiusFibonacci + 2 * arcSpacing * 3, radiusFibonacci + 2 * arcSpacing * 2, 185, 50);
-        g.drawArc(centerX - 180, centerY - 20 - arcSpacing, radiusFibonacci + 3 * arcSpacing * 6, radiusFibonacci + 3 * arcSpacing * 2, 190, 50);
-        //g.drawArc(centerX - 130, centerY - 65, radiusFibonacci + arcSpacing * 2, radiusFibonacci + arcSpacing * 2, -80, -80);
+        g.drawArc(centerX - 104, centerY - 80, radiusFibonacci + arcSpacing * 2, radiusFibonacci + arcSpacing * 2, 180, 50);
+        g.drawString("40", centerX - 120, centerY + 35);
+        g.drawArc(centerX - 135, centerY - 70 - arcSpacing, radiusFibonacci + 2 * arcSpacing * 3, radiusFibonacci + 3 * arcSpacing * 2, 185, 50);
+        g.drawString("50", centerX - 150, centerY + 80);
+        g.drawArc(centerX - 160, centerY - 20 - arcSpacing, radiusFibonacci + 3 * arcSpacing * 6, radiusFibonacci + 4 * arcSpacing * 2, 185, 50);
+        g.drawString("60", centerX - 190, centerY + 115);
+        
 
         double angle;
         // Hours
         ((Graphics2D) g).setStroke(new BasicStroke(initialStrokeWidth));
-        g.setColor(Color.white);
         angle = Math.toRadians((15 - (hour % 12) - (minute / 60.0) - (second / 3600.0)) * 30);
         int startX = centerX + (int) (Math.cos(angle) * circleRadius);
         int startY = centerY - (int) (Math.sin(angle) * circleRadius);
-        int otherX = centerX + (int) (Math.cos(angle) * (circleRadius + 20));
-        int otherY = centerY - (int) (Math.sin(angle) * (circleRadius + 20));
+        int otherX = centerX + (int) (Math.cos(angle) * (circleRadius + 50));
+        int otherY = centerY - (int) (Math.sin(angle) * (circleRadius + 50));
+        g.setColor(Color.green);
         g.drawLine(startX, startY, otherX, otherY);
 
         // Minutes
         ((Graphics2D) g).setStroke(new BasicStroke(initialStrokeWidth));
         angle = Math.toRadians((15 - minute - (second / 60.0)) * 6);
+        g.setColor(Color.white);
         g2d = (Graphics2D) g.create();
         g2d.rotate(-angle, centerX, centerY);
         if (spiralImageWhite != null) {
@@ -159,8 +156,8 @@ public class FibonacciClock extends JFrame implements Runnable {
         ((Graphics2D) g).setStroke(new BasicStroke(2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         g.setColor(Color.red);
         angle = Math.toRadians((15 - second - (millisecond / 1000.0)) * 6);
-        x = (int) (Math.cos(angle) * (radius - 20));
-        y = (int) (Math.sin(angle) * (radius - 10)); // smaller < taller
+        x = (int) (Math.cos(angle) * radius);
+        y = (int) (Math.sin(angle) * radius); 
         int startXS = centerX - x / 5; // Oposite position on X
         int startYS = centerY + y / 5; // Oposite position on Y
         g.drawLine(centerX, centerY, centerX + x, centerY - y);
